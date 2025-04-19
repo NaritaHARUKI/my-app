306b9569d78778ef50fbda4681baf7c24bae3447
成田陽紀 <naritaharuki@naritaharuki.local>
2025-04-20 00:46:56 +0900

    initial commit

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import 'dotenv/config'
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
-  password: process.env.DB_USER_PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
   waitForConnections: true,
