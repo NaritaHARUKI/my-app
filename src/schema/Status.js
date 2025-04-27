5ad3d06629b0e6ada248e50fbd107ec7b4eec499
成田陽紀 <naritaharuki@naritaharuki.local>
2025-04-27 23:05:29 +0900

    Revert "ビルド"
    This reverts commit a7a7110ae612390931e4fdf6d656f8be953d4ad5.

--- src/schema/Status.js
+++
@@ -1,8 +0,0 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core';
-export const status = mysqlTable('status', (table) => {
-    return {
-        lineId: varchar('lineId', { length: 255 }).primaryKey(),
-        shopStatus: varchar('shop_status', { length: 255 }),
-        merchandiseStatus: varchar('merchandise_status', { length: 255 }),
-    };
-});
--- src/schema/User.js
+++
@@ -1,8 +0,0 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core';
-export const users = mysqlTable('users', (table) => {
-    return {
-        lineId: varchar('lineId', { length: 255 }).primaryKey(),
-        name: varchar('name', { length: 255 }),
-        type: varchar('type', { length: 255 }),
-    };
-});
