a40f32e39374557d20d7671339ca984b46061d71
成田陽紀 <naritaharuki@naritaharuki.local>
2025-04-29 23:32:37 +0900

    userstationsはプライマリキーないので削除

--- src/schema/UserStations.ts
+++ src/schema/UserStations.ts
@@ -2,7 +2,7 @@ import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
 
 export const userStaions = mysqlTable('users_stations', (table) => {
   return {
-    lineId: varchar('lineId', { length: 255 }).primaryKey(),
+    lineId: varchar('lineId', { length: 255 }),
     stationId: int('stationId').notNull(),
   }
 })
